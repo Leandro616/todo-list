8d34c06772d25aca3992fb9f9ab032137d4cb502
Leandro616 <leandrosr616@gmail.com>
2021-10-29 11:38:54 -0300

    adicionada a função de deletar entidades realacionadas com Cascade ALL do JPA

--- src/main/java/io/github/leandro616/todolist/model/entity/ListaDeTarefas.java
+++ src/main/java/io/github/leandro616/todolist/model/entity/ListaDeTarefas.java
@@ -1,5 +1,9 @@
 package io.github.leandro616.todolist.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -27,4 +32,7 @@ public class ListaDeTarefas {
    @ManyToOne
    @JoinColumn(name = "id_usuario")
    private Usuario usuario;
+
+   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "lista")
+   private List<Tarefa> tarefas = new ArrayList<>();
 }
